9f3962989152540b676ce6b188e8bc1bbc5b165d
Vinicius Egidio <me@vinicius.io>
2025-10-19 14:12:01 +0200

    refactor: Add initial layout for the PreviewEmpty component

--- gui/frontend/src/main.tsx
+++ gui/frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeInit } from '../.flowbite-react/init.tsx';
 import { Home } from './Home.tsx';
 import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
+        <ThemeInit />
         <Home />
     </React.StrictMode>,
 );
